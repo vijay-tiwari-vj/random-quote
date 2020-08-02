4673203f618745cc759c963e02d1c82db9d6a3ec
Vijay Tiwari <vijay.tiwari.vj7>
2020-08-02 14:16:03 +0530

    Updates

---
+++ precache-manifest.0151c39d6c394ee9666c085d584c635d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3adc14eae3b0eae5dc89f0e80e7c03b5",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "044196ee270d12ff3729",
+    "url": "/random-quote/static/css/main.ad68b31f.chunk.css"
+  },
+  {
+    "revision": "df32ecdc257e3b798235",
+    "url": "/random-quote/static/js/2.00ffbf15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote/static/js/2.00ffbf15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044196ee270d12ff3729",
+    "url": "/random-quote/static/js/main.81d92559.chunk.js"
+  },
+  {
+    "revision": "281c3fdc18e22c6a427d",
+    "url": "/random-quote/static/js/runtime-main.ce5e683c.js"
+  }
+]);
